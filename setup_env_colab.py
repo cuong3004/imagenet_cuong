ba7bcf57f3cf656abd856795311d606f0e23f6d8
cuong3004 <duongduccuong30042002@gmail.com>
2023-02-21 09:58:40 +0700

    update

---
+++ setup_env_colab.py
@@ -0,0 +1,13 @@
+import urllib.request
+import zipfile
+import os
+
+url = 'https://github.com/tensorflow/models/archive/refs/tags/v2.11.0.zip'
+file_name = 'v2.11.0.zip'
+
+urllib.request.urlretrieve(url, file_name)
+
+with zipfile.ZipFile(file_name, 'r') as zip_ref:
+    zip_ref.extractall()
+
+os.rename('models-2.11.0', 'models')
